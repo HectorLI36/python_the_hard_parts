04155912450ded1abfb48891745152d767df514d
hectorl <hectorl@nvidia.com>
2020-06-05 12:03:11 +0800

    Reformat abc.py

--- abc.py
+++ abc.py
@@ -4,50 +4,50 @@ import six
 
 @six.add_metaclass(abc.ABCMeta)
 class BaseClass(object):
-    @abc.abstractmethod
-    def func_a(self, data):
-        """
-        an abstract method need to be implemented
-        """
+    @abc.abstractmethod
+    def func_a(self, data):
+        """
+        an abstract method need to be implemented
+        """
 
-    @abc.abstractmethod
-    def func_b(self, data):
-        """
-        another abstract method need to be implemented
-        """
+    @abc.abstractmethod
+    def func_b(self, data):
+        """
+        another abstract method need to be implemented
+        """
 
 class SubclassImpl(BaseClass):  # 不行
-    def func_a(self, data):
-        print("Overriding func_a, " + str(data))
+    def func_a(self, data):
+        print("Overriding func_a, " + str(data))
 
-    @staticmethod
-    def func_d(self, data):
-        print(type(self) + str(data))
+    @staticmethod
+    def func_d(self, data):
+        print(type(self) + str(data))
 
 class RegisteredImpl(object):  # 可以不实现
-    @staticmethod
-    def func_c(data):
-        print("Method in third-party class, " + str(data))
+    @staticmethod
+    def func_c(data):
+        print("Method in third-party class, " + str(data))
 
 BaseClass.register(RegisteredImpl)
 
 
 if __name__ == '__main__':
-    for subclass in BaseClass.__subclasses__():
-        print("subclass of BaseClass: " + subclass.__name__)
-    print("subclass do not contains RegisteredImpl")
-    print("-----------------------------------------------")
+    for subclass in BaseClass.__subclasses__():
+        print("subclass of BaseClass: " + subclass.__name__)
+    print("subclass do not contains RegisteredImpl")
+    print("-----------------------------------------------")
 
-    print("RegisteredImpl is subclass: " + str(issubclass(RegisteredImpl, BaseClass)))
-    print("RegisteredImpl object  is instance: " + str(isinstance(RegisteredImpl(), BaseClass)))
-    print("SubclassImpl is subclass: " + str(issubclass(SubclassImpl, BaseClass)))
+    print("RegisteredImpl is subclass: " + str(issubclass(RegisteredImpl, BaseClass)))
+    print("RegisteredImpl object  is instance: " + str(isinstance(RegisteredImpl(), BaseClass)))
+    print("SubclassImpl is subclass: " + str(issubclass(SubclassImpl, BaseClass)))
 
-    print("-----------------------------------------------")
-    obj1 = RegisteredImpl()
-    obj1.func_c("RegisteredImpl new object OK!")
-    print("-----------------------------------------------")
-    obj2 = SubclassImpl()  #由于没有实例化所有的方法，所以这里会报错 Can't instantiate abstract class SubclassImpl with abstract methods func_b
-    obj2.func_a("It's right!")
+    print("-----------------------------------------------")
+    obj1 = RegisteredImpl()
+    obj1.func_c("RegisteredImpl new object OK!")
+    print("-----------------------------------------------")
+    obj2 = SubclassImpl()  #由于没有实例化所有的方法，所以这里会报错 Can't instantiate abstract class SubclassImpl with abstract methods func_b
+    obj2.func_a("It's right!")
 
 """
 subclass of BaseClass: SubclassImpl
